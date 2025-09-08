c08e06fd3708301cd9fa70d328096ad30edacbaf
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-09-08 12:06:15 -0300

    xp

--- cansi/mingw/01-ex/build.sh
+++ cansi/mingw/01-ex/build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-BAKEFILE=/home/ivan/wx/linux-wx-3.2.4/bin/bakefile
-
-PYENV_VERSION=2.7.18 \
-    $BAKEFILE \
-    -f mingw hello.bkl \
-    -o Makefile
+mingw32-make.exe -f makefile.gcc
 
 exit 0
